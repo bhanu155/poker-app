a6937f9dcc3f00a5112c30c2ed14205c5e356a92
Jens Keller <jens.keller@sap.com>
2019-12-02 12:16:27 +0100

    updated spring version + small adjustments

--- src/main/resources/static/table/test/allTests.js
+++
@@ -1,5 +0,0 @@
-jQuery.sap.require("sap.ui.qunit.qunit-css");
-jQuery.sap.require("sap.ui.thirdparty.qunit");
-jQuery.sap.require("sap.ui.test.Opa5");
-jQuery.sap.require("sap.ui.test.opaQunit");
-jQuery.sap.require("poker.test.AliceAndBobsPokerJourney");
